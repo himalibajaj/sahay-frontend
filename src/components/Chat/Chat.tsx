e1fb096263d434971c9768e8a4b81540facaa29e
Jasraj Singh Bhatia <singhbhatiajasraj@gmail.com>
2023-04-23 16:58:46 +0530

    minor change

--- src/components/Chat/Chat.tsx
+++ src/components/Chat/Chat.tsx
@@ -29,7 +29,7 @@ const MessageBox = ({
             }}
             value={message}
             type="text"
-            className="mr-3 px-3.5 !outline-none block w-full rounded-md border-0  py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-600 sm:text-sm sm:leading-6"
+            className="appearance-none mr-3 px-3.5 !outline-none block w-full rounded-md border-0  py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-600 sm:text-sm sm:leading-6"
           />
           <button
             disabled={message === ""}
